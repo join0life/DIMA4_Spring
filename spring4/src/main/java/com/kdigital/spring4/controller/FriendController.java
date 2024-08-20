dbd2537625626af872ab9644ef085c7fdf64eda7
join0life <158464763+join0life@users.noreply.github.com>
2024-08-20 17:49:30 +0900

    Add files via upload

---
+++ spring4/src/main/java/com/kdigital/spring4/controller/FriendController.java
@@ -0,0 +1,20 @@
+package com.kdigital.spring4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.kdigital.spring4.dto.Friend;
+
+@Controller
+public class FriendController {
+	@PostMapping("/regist")
+	public String regist(
+			@ModelAttribute Friend friend,
+			Model modle ){
+		System.out.println(friend);
+		
+		return "registResult";
+	}
+}
