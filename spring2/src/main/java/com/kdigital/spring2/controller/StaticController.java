2bbb30ffc8dc8756309a56357a3cc06726598104
join0life <158464763+join0life@users.noreply.github.com>
2024-08-20 17:47:14 +0900

    Add files via upload

---
+++ spring2/src/main/java/com/kdigital/spring2/controller/StaticController.java
@@ -0,0 +1,24 @@
+package com.kdigital.spring2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StaticController {
+	
+	@GetMapping("/image") // localhost:9000/image
+	public String image() {
+		return "image";
+	}
+	
+	@GetMapping("/css") 
+	public String css() {
+		return "css";
+	}
+	
+	@GetMapping("/javascript") 
+	public String javascript() {
+		return "javascript";
+	}
+	
+}
