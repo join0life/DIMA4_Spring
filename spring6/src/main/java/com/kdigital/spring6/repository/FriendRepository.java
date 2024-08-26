57963d860d3cb9fbc1b2eeb09971eaba7656b966
join0life <158464763+join0life@users.noreply.github.com>
2024-08-26 18:24:44 +0900

    Delete spring6 directory

--- spring6/src/main/java/com/kdigital/spring6/repository/FriendRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kdigital.spring6.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kdigital.spring6.entity.FriendEntity;
-
-public interface FriendRepository extends JpaRepository<FriendEntity, Integer> {
-
-}
