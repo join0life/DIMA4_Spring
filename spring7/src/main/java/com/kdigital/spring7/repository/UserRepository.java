e6fb02e5151922d8beb4d226a41fd4a9415e316e
join0life <158464763+join0life@users.noreply.github.com>
2024-09-11 17:50:39 +0900

    Delete spring7/src directory

--- spring7/src/main/java/com/kdigital/spring7/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kdigital.spring7.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kdigital.spring7.entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, String> {
-
-}
