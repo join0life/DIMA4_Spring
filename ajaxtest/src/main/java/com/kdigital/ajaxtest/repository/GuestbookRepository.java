df4cb122c4b65051c117dc5d0ba00b1b8d5653c9
join0life <158464763+join0life@users.noreply.github.com>
2024-09-24 21:19:04 +0900

    Delete ajaxtest directory

--- ajaxtest/src/main/java/com/kdigital/ajaxtest/repository/GuestbookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kdigital.ajaxtest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kdigital.ajaxtest.entity.GuestbookEntity;
-
-public interface GuestbookRepository extends JpaRepository<GuestbookEntity, Long> {
-
-}
